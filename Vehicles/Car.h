13c135aad83f9e4f9332eaa35ca1166c384e395c
PrinceFroggy <asolesa@yahoo.ca>
2017-03-11 11:05:46 -0500

    Go!

---
+++ Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Andrew Solesa on 2017-03-11.
+//  Copyright © 2017 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
